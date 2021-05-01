c279f2e477ed6118407f67fe774bb733217d7073
datagutten <datagutten@datagutten.net>
2021-05-01 18:05:58 +0200

    Add data tables to parent table

---
+++ addon/PetDataTables.lua
@@ -0,0 +1,11 @@
+_G['PetInfo'] = {
+    AbilitySource = _G['PetAbilitySource'],
+    PetFamilies = _G['PetFamilies'],
+    PetProperties = _G['PetProperties'],
+    PetFamilyMembers = _G['PetFamilyMembers'],
+    SpellIconTextures = _G['PetSpellIconTextures'],
+    SpellProperties = _G['PetSpellProperties'],
+    SpellRanks = _G['PetSpellRanks'],
+    Zones = _G['ZoneInfo'],
+    ZonesNameToId = _G['ZonesNameToId'],
+}
