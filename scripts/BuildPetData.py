954c50f7828ac15d335bea821e9930bd61fd910e
datagutten <datagutten@datagutten.net>
2021-05-01 17:24:21 +0200

    Add new classes to build data

---
+++ scripts/BuildPetData.py
@@ -0,0 +1,150 @@
+import json
+import os
+import re
+
+from scripts import Petopia
+from utils.Wowhead import Wowhead
+
+
+class BuildPetData(Wowhead):
+    def __init__(self):
+        data_folder = os.path.join(os.path.dirname(__file__), '..', 'data')
+        super(BuildPetData, self).__init__(data_folder)
+        self.petopia = Petopia()
+
+    def families(self):
+        textures = self.art_textures()
+        response = self.query(uri='/hunter-pets')
+        families = self.get_list_view(response, 'pets')
+
+        family_ids = []
+        pet_spells = []
+
+        data_dict = {}
+        for family in families:
+            del family['popularity']
+            pet_spells.append(family['spells'])
+            family_ids.append(family['id'])
+            family['icon_texture'] = textures[family['icon']]
+            data_dict[family['id']] = family
+
+        return data_dict
+
+    def passive_spells(self):
+        response = self.query(uri='/spells/pet-abilities/hunter?filter=50;1;0')
+        matches = re.search(r'WH\.Gatherer\.addData\(.+?({.+})\);', response.text)
+        spells = json.loads(matches.group(1))
+        spell_list = []
+        for spell_id, spell in spells.items():
+            if spell['icon'] not in spell_list:
+                spell_list.append(spell['icon'])
+        return spell_list
+
+    def spell_properties(self):
+        passive_spells = self.passive_spells()
+        response = self.query(uri='/spells/pet-abilities/hunter')
+
+        properties = {}
+        spell_icon_textures = {}
+
+        spells = self.get_gatherer_data(response)
+        textures = self.art_textures()
+        spell_sources = self.petopia.spells()
+
+        for spell in self.get_list_data(response, 'listviewspells'):
+            if 'chrclass' in spell:
+                if spell['chrclass'] != 4:
+                    continue
+                del spell['chrclass']
+            del spell['popularity']
+
+            if 'rank' in spell:
+                matches = re.match(r'\w+\s([0-9]+)', spell['rank'])
+                if not matches:
+                    continue
+                spell['rank'] = int(matches.group(1))
+            spell['icon'] = spells[str(spell['id'])]['icon']
+            spell['icon_texture'] = textures[spell['icon']]
+            spell['passive'] = spell['icon'] in passive_spells
+            try:
+                spell['source'] = spell_sources[spell['icon']][spell['rank']]
+            except KeyError as e:
+                # print('Invalid key:', e)
+                pass
+
+            properties[spell['id']] = spell
+            spell_icon_textures[spell['icon_texture']] = spell['icon']
+
+        return properties, spell_icon_textures, spell_sources
+
+    def spell_ranks(self):
+        response = self.query(uri='/spells/pet-abilities/hunter')
+        spells = self.get_gatherer_data(response)
+        ranks = {}
+
+        for spell_id, spell in spells.items():
+            icon = spell['icon']
+            rank_key = 'rank_enus'
+
+            if spell[rank_key] == '':
+                rank = 1
+            else:
+                matches = re.match(r'\w+\s([0-9]+)', spell[rank_key])
+                if not matches:
+                    continue
+                rank = int(matches.group(1))
+
+            if icon not in ranks:
+                ranks[icon] = {}
+
+            ranks[icon][rank] = int(spell_id)
+        return ranks
+
+    def pet_properties(self):
+        with open(self.file_name('PetFamilies', 'json')) as fp:
+            families = json.load(fp)
+        pets = {}
+        family_members = {}
+        for family in families.keys():
+            family = int(family)
+            response = self.query(uri='/pet=%d' % family)
+            data = self.get_list_view(response, 'tameable')
+            for npc in data:
+                if 'location' not in npc:
+                    continue
+                pets[npc['id']] = npc
+                if family not in family_members:
+                    family_members[family] = []
+                family_members[family].append(npc['id'])
+        return pets, family_members
+
+    def zones(self):
+        response = self.query(uri='/zones')
+        zones = self.get_list_view(response, 'zones')
+        zones_out = {}
+        zones_name_to_id = {}
+        for zone in zones:
+            del zone['popularity']
+            zones_out[zone['id']] = zone
+            zones_name_to_id[zone['name']] = zone['id']
+        return zones_out, zones_name_to_id
+
+
+if __name__ == "__main__":
+    build = BuildPetData()
+    build.save(build.families(), 'PetFamilies')
+
+    spell_properties, icon_textures, sources = build.spell_properties()
+    build.save(spell_properties, 'PetSpellProperties')
+    build.save(icon_textures, 'PetSpellIconTextures')
+
+    build.save(sources, 'PetAbilitySource')
+    build.save(build.spell_ranks(), 'PetSpellRanks')
+
+    pet_properties, pet_family_members = build.pet_properties()
+    build.save(pet_properties, 'PetProperties')
+    build.save(pet_family_members, 'PetFamilyMembers')
+
+    zone_info, zone_name_to_id = build.zones()
+    build.save(zone_info, 'ZoneInfo')
+    build.save(zone_name_to_id, 'ZonesNameToId')
---
+++ scripts/Petopia.py
@@ -0,0 +1,121 @@
+import re
+
+from lxml import html as html
+
+from build_utils import WoWBuildUtils
+
+
+class Petopia(WoWBuildUtils):
+    def __init__(self, tbc=False):
+        super().__init__()
+        if tbc:
+            self.abilities_url = 'https://www.wow-petopia.com/classic_bc/abilities.php'
+        else:
+            self.abilities_url = 'https://www.wow-petopia.com/classic/abilities.php'
+
+    def spells(self):
+        response = self.get(self.abilities_url)
+        root = html.fromstring(response.text)
+
+        ability_ranks = {}
+        abilities = root.xpath('//ul[@class="abilityranklist classic"]')
+        for ability in abilities:
+            # header = ability.find('h3[@class="guide_heading classic"]')
+            icon = None
+            for rank in ability.findall('li'):
+                rank_id = rank.get('id')
+                matches = re.match(r'([a-z]+)([0-9]+)', rank_id)
+                ability_key = matches[1]
+                rank_num = int(matches[2])
+                if not icon:
+                    icon = root.xpath('//h3[@id="%s"]/img' % ability_key)[0].get('src')
+                    icon = re.sub(r'.+/(.+)\.png', r'\1', icon)
+                if icon == 'ability_druid_ferociousbite':
+                    icon = 'ability_racial_cannibalize'
+
+                # print('Rank key: %s Rank num: %d' % (ability_key, rank_num))
+                if icon not in ability_ranks:
+                    ability_ranks[icon] = {}
+                ability_ranks[icon][rank_num] = []
+
+                ul = rank.find('ul[@class="abilityranknpclist classic"]')
+                if ul is None:  # Trainer?
+                    header = rank.find('span[@class="abilitysourceheading classic"]')
+                    if header.text == 'Can be learned from trainers.':
+                        ability_ranks[icon][rank_num] = 'trainer'
+                    elif header.text == 'No known training source.':
+                        ability_ranks[icon][rank_num] = 'unknown'
+                    else:
+                        raise ValueError('Invalid status: ' + header.text)
+                    continue
+
+                for npc in ul.findall('li'):
+                    npc_link = npc.find('a').get('href')
+                    matches = re.search(r'npc=([0-9]+)', npc_link)
+                    npc_id = int(matches[1])
+                    ability_ranks[icon][rank_num].append(npc_id)
+        return ability_ranks
+
+
+def parse_petopia():
+    file = open('Petopia BC Classic_ Pet Family Abilities.html', 'r')
+    root = html.fromstring(file.read())
+
+    ranks = {}
+    abilities = root.xpath('//ul[@class="abilityranklist classic"]')
+    for ability in abilities:
+        for rank in ability.findall('li'):
+            rank_id = rank.get('id')
+            matches = re.match(r'([a-z]+)([0-9]+)', rank_id)
+            rank_key = matches[1]
+            rank_num = int(matches[2])
+            # print('Rank key: %s Rank num: %d' % (rank_key, rank_num))
+            if rank_key not in ranks:
+                ranks[rank_key] = {}
+            ranks[rank_key][rank_num] = []
+            # ranks[rank_key].append([])
+
+            ul = rank.find('ul[@class="abilityranknpclist classic"]')
+            if ul is None:  # Trainer?
+                header = rank.find('span[@class="abilitysourceheading classic"]')
+                # if header.text == 'Can be learned from trainers.':
+                ranks[rank_key][rank_num] = header.text
+                # ranks[rank_key].append(header.text)
+                # print(header.text)
+                continue
+            # else:
+            #    ranks[rank_key].append([])
+
+            for npc in ul.findall('li'):
+                npc_link = npc.find('a').get('href')
+                matches = re.search(r'npc=([0-9]+)', npc_link)
+                npc_id = int(matches[1])
+                # print(npc_id)
+                ranks[rank_key][rank_num].append(npc_id)
+                # ranks[rank_key].append(npc_id)
+
+                # print('class', npc.get('class'))
+    return ranks
+
+
+def parse_skill_names(file_name='Petopia Classic_ Pet Family Abilities.html'):
+    file = open(file_name, 'r')
+    root = html.fromstring(file.read())
+    headers = root.xpath('.//h4[@class="guide_index_heading classic"]')
+
+    skills = {}
+    for header in headers:
+        header_text = header.text.strip()
+        if header_text == 'Learned from wild creatures:':
+            source = 'wild'
+        else:
+            source = 'trainer'
+
+        for skill in header.find('ul').findall('li'):
+            skill_key = skill.xpath('a/@href')[0]
+            skill_key = skill_key[1:]
+            skill_name = str(skill.xpath('a/text()')[0])
+            print('Key: %s Name: %s' % (skill_key, skill_name))
+            skills[skill_key] = {'key': skill_key, 'name': skill_name, 'source': source}
+
+    return skills
