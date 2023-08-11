332de96a8c070d72abf5c12863940e6af6729835
datagutten <datagutten@datagutten.net>
2023-08-11 14:50:19 +0200

    Get game version from environment variable

--- scripts/BuildMapData.py
+++ scripts/BuildMapData.py
@@ -3,5 +3,6 @@ import os
 from build_utils.utils.BuildMapData import BuildMapData
 
 data_folder = os.path.join(os.path.dirname(__file__), '..', 'data')
-build = BuildMapData(data_folder=data_folder)
+version = os.getenv('GAME_VERSION')
+build = BuildMapData(data_folder=data_folder, game=version)
 build.save(build.map_to_area(), 'MapToZone')
